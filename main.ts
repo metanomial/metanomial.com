dac58731b4ff823962d102eb8b631fc08fd9f4db
Benjamin Herman <benjamin@metanomial.com>
2021-03-30 17:22:50 -0700

    Cleanup code

--- main.ts
+++ main.ts
@@ -1,19 +1,29 @@
-import { serve } from 'https://deno.land/std@0.80.0/http/server.ts'
-import { respondIndexHTML } from './index.html.ts'
+import { serve, Response } from 'https://deno.land/std@0.91.0/http/server.ts'
 
 const port = Number(Deno.env.get('PORT') ?? 8080)
-if (Number.isNaN(port)) throw new TypeError('Invalid port value')
+if (Number.isNaN(port))
+  throw new TypeError('Invalid port value')
 
 const server = serve({ port })
 console.log(`Serving on port ${port}`)
 
+const Response: Record<string, Response> = {
+  INDEX: {
+    headers: new Headers({ 'Content-Type': 'text/html' }),
+    body: await Deno.readTextFile('./index.html')
+  },
+  NOT_FOUND: {
+    status: 404
+  }
+}
+
 for await (const request of server) {
-	switch (request.url) {
-		case '/':
-		case '/index.html':
-			respondIndexHTML(request)
-			break
-		default:
-			request.respond({ status: 404 })
-	}
+  switch (request.url) {
+    case '/':
+    case '/index.html':
+      request.respond(Response.INDEX)
+      break
+    default:
+      request.respond(Response.NOT_FOUND)
+  }
 }
