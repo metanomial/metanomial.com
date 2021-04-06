30a9d225e0480793ac48b121659505aedd92e0d1
Benjamin Herman <benjamin@metanomial.com>
2021-04-06 00:50:53 -0700

    Change to be compatible with Deploy

--- main.ts
+++ main.ts
@@ -1,29 +1,17 @@
-import { serve, Response } from 'https://deno.land/std@0.91.0/http/server.ts'
-
-const port = Number(Deno.env.get('PORT') ?? 8080)
-if (Number.isNaN(port))
-  throw new TypeError('Invalid port value')
-
-const server = serve({ port })
-console.log(`Serving on port ${port}`)
-
-const Responses: Record<string, Response> = {
-  INDEX: {
-    headers: new Headers({ 'Content-Type': 'text/html' }),
-    body: await Deno.readTextFile('./index.html')
-  },
-  NOT_FOUND: {
-    status: 404
-  }
-}
-
-for await (const request of server) {
+async function handleRequest (request: Request) {
   switch (request.url) {
     case '/':
-    case '/index.html':
-      request.respond(Responses.INDEX)
-      break
+    case '/index.html': {
+      const url = new URL('index.html', import.meta.url)
+      const response = await fetch(url)
+      response.headers.set('Content-Type', 'text/html; charset=utf-8')
+      return response
+    }
     default:
-      request.respond(Responses.NOT_FOUND)
+      return new Response(null, { status: 404 })
   }
 }
+
+addEventListener('fetch', (event: FetchEvent) => {
+  event.respondWith(handleRequest(event.request))
+})
